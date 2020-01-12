6f09b2ae52481d8790483702d8671c3eaed363ab
jun_mac <junwatanabe72@gmail.com>
2020-01-12 16:20:43 +0900

    component

--- sample/src/App.js
+++ sample/src/App.js
@@ -1,14 +1,22 @@
 import React,{Component} from 'react';
 
+
 class App extends Component{
   render(){
     return(
       <React.Fragment>
+      <Cat />
+      <Cat />
+      <Cat />
         <label htmlFor="bar">bar</label>
         <input type="text" onClick={() =>{console.log("I am clicked.")}} />
       </React.Fragment>
     )
   }
+}
+
+const Cat = ()=>{
+  return <div>meow!</div>
 }
 
 export default App;

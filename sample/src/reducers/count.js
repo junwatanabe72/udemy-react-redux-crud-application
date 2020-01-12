4ebe99bb6adf843ad9b96d01163eb9ad48180f32
jun_mac <junwatanabe72@gmail.com>
2020-01-12 18:31:43 +0900

    implement redux store

--- sample/src/reducers/count.js
+++ sample/src/reducers/count.js
@@ -6,9 +6,9 @@ export default (state = initalState, action) => {
   switch (action.type)  {
     case INCREMENT:
       return {value: state.value + 1}
-      case INCREMENT:
-        return {value: state.value - 1}
-      default:
-        return state
+    case INCREMENT:
+      return {value: state.value - 1}
+    default:
+      return state
   }
 }
